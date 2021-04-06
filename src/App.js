d4701cad3567cf6505e8c85d729c7fb8eca8b225
efloden <earl.mark.27@gmail.com>
2021-04-06 18:13:38 +0900

    :camera: implement pokeapi service worker to ensure all requests are cached

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import React from 'react'
+
+const Pokedex = require("pokeapi-js-wrapper")
+const P = new Pokedex.Pokedex({ cacheImages: true })
+
 function App() {
   return (
-    null
+    <div id="app">
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg"
+        alt="Pikachu"
+      />
+      <div>
+        The above image is served (once cached) by the local <i>pokeapi-js-wrapper-sw.js</i> SW. Check it out in the Developer Console (F12) by looking at the Network Panel and also at the Application Panel.
+      </div>
+    </div>
   );
 }
 
